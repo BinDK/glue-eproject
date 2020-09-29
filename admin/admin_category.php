0807c0ed1baeb5fb3cc3725edcc0cf8ecc7d9f29
phuc hung <nguyenphuchung09@gmail.com>
2020-09-29 22:57:59 +0700

    version1 admin_category.php

--- admin/admin_category.php
+++ admin/admin_category.php
@@ -1,21 +1,39 @@
-<?php 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<?php
 require_once 'connect.php';
-$result = mysqli_query($con,'select * from db_category');
- ?>
-
-<table>
-	<tr>
+if(isset($_POST['buttonAdd'])){
+	$name = $_POST['species_name'];
+	$description = $_POST['description'];
+	mysqli_query($con,'insert into db_category(species_name,description) values ("'.$name.'","'.$description.'")');
+	header('location:admin_category.php');
+}
+$result = mysqli_query($con, 'select * from db_category');
+?>
+<form action="admin_category.php" method="post" style="width:300px">
+  <div class="form-group">
+    <label for="species_name">Category's name:</label>
+    <input type="text" class="form-control" name="species_name">
+  </div>
+  <div class="form-group">
+    <label for="description">Description:</label>
+    <textarea name="description" cols="20" rows="5"></textarea>
+  </div>
+  <input type="submit" name="buttonAdd" value="Add" id="created">
+</form>
+<table class="table table-bordered" style="width:800px">
+	<tr style="text-align:center">
 		<th>ID</th>
 		<th>Species Name</th>
+		<th>Description</th>
 	</tr>
-<?php while ($category = mysqli_fetch_array($result)) { ?>
- 			<tr>
- 				<td><?= $category['id'] ?></td>
- 				<td align="center"><?= $category['species_name'] ?></td>
- 				<td align="center"><a href="detail_Product.php?id=<?= $category['id']?>">Detail</a> |
- 				<a href="edit_product.php?id=<?= $category['id'] ?>">Edit</a> |
- 				<a onclick="return confirm('Are you really want to delete it??')" href="demo4.php?id=<?= $category['id']?>&action=delete">Delete</a>
- 				</td>
- 			</tr>
- 	<?php } ?>
+	<?php while ($category = mysqli_fetch_array($result)) { ?>
+		<tr>
+			<td><?= $category['id'] ?></td>
+			<td align="center"><?= $category['species_name'] ?></td>
+			<td align="center"><?= $category['description'] ?></td>
+		</tr>
+	<?php } ?>
 </table>

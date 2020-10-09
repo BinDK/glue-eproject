6506a486227e193510aa816f47dfb2f9f17232ba
BinDK <sarakenny123@yahoo.com>
2020-10-09 22:44:40 +0700

    zoo/random de test 404

---
+++ 404.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>	
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="bootstrap.min.css">
+<script src="admin/js/jquery-3.5.1.min.js"></script>
+<script src="admin/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+<link rel="stylesheet" href="style.css">
+	<style>
+		body{
+			background: url('../KTPHP/neon_inkscape.svg') ;
+		}
+		h1{
+			position: absolute;
+			top: 300px;
+			left: 500px;
+		animation-name: quay;
+		animation-duration: 50ms;
+		animation-iteration-count: 20;
+		animation-timing-function: linear; 
+		}
+		@keyframes quay {
+    	from {
+        transform:rotate(0deg);
+    	}
+    	to {
+        transform:rotate(360deg);
+    	}
+		}
+	</style>
+</head>
+<body>
+	<?php require_once 'header.php'; ?>
+	<h1>Đang suy nghĩ thêm..........</h1>
+</body>
+</html>
